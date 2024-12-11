5bc91366631b4ca0e4fabd6e5b2050b915103bdc
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2024-12-11 07:33:39 +0530

    Time: 40 ms (53.3%), Space: 60.3 MB (50%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.java
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.java
@@ -5,19 +5,12 @@ class Solution {
         Arrays.sort(nums);
         int val=0;
         for(int r=0;r<nums.length;r++){
-            int a=nums[l];
-            int b=nums[r];
-            if(b-k<=a+k){
-                val++;
+            while(l<nums.length && nums[r]-k>nums[l]+k){
+                val--;
+                l++;
             }
-            else{
-                while(l<nums.length && nums[r]-k>nums[l]+k){
-                    val--;
-                    l++;
-                }
-                if(l<nums.length && nums[r]-k<=nums[l]+k){
-                    val++;
-                }
+            if(l<nums.length && nums[r]-k<=nums[l]+k){
+                val++;
             }
             ans=Math.max(ans,val);
         }
