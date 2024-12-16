7e26d81991dee9da113312dace393f2d10c86c79
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2024-12-16 07:24:51 +0530

    Time: 1 ms (100%), Space: 45.1 MB (20.61%) - LeetHub

--- 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
+++ 3555-final-array-state-after-k-multiplication-operations-i/3555-final-array-state-after-k-multiplication-operations-i.java
@@ -1,19 +1,16 @@
 class Solution {
-    public int[] getFinalState(int[] nums, int k, int multiplier) {
-        PriorityQueue<Integer> heap=new PriorityQueue();
-        for(int i:nums){
-            heap.offer(i);
-        }
-        while(k-- >0){
-            int val=heap.poll();
-            for(int i=0;i<nums.length;i++){
-                if(val==nums[i]){
-                    nums[i]=nums[i]*multiplier;
-                    heap.offer(nums[i]);
-                    break;
+    public int[] getFinalState(int[] arr, int k, int multiplier) {
+        int ans[] = new int[arr.length];
+        while (k > 0) {
+            int x = 0;
+            for (int i = 1; i < arr.length; i++) {
+                if (arr[i] < arr[x]) {
+                    x = i;
                 }
             }
+            arr[x] = arr[x] * multiplier;
+            k--;
         }
-        return nums;
+        return arr;
     }
 }
