44839eda4330839a0c1e70cd31858007f3a94192
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2024-12-23 08:20:03 +0530

    Time: 714 ms (8.9%), Space: 66.1 MB (66.44%) - LeetHub

---
+++ 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level.java
@@ -0,0 +1,84 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int minimumOperations(TreeNode root) {
+        if(root==null){
+            return 0;
+        }
+        int ans=0;
+        Queue<TreeNode> queue=new LinkedList();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int n=queue.size();
+            int[] arr=new int[n];
+            for(int i=0;i<n;i++){
+                TreeNode node=queue.poll();
+                arr[i]=node.val;
+                if(node.left!=null){
+                    queue.add(node.left);
+                }
+                if(node.right!=null){
+                    queue.add(node.right);
+                }
+            }
+            if(sorted(arr)){
+                continue;
+            }
+            ans+=minSwaps(arr,n);
+        }
+        return ans;
+    }
+    boolean sorted(int[] arr){
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]){
+                return false;
+            }
+        }
+        return true;
+    }
+    private int minSwaps(int[] arr, int N) {
+        int ans = 0;
+        int[] temp = Arrays.copyOfRange(arr, 0, N);
+        Arrays.sort(temp);
+
+        for (int i = 0; i < N; i++) {
+            if (arr[i] != temp[i]) {
+                ans++;
+                int z=0;
+                for(int j=0;j<N;j++){
+                    if(arr[j]==temp[i]){
+                        z=j;
+                        break;
+                    }
+                }
+                swap(arr, i, z);
+            }
+        }
+        return ans;
+    }
+    private int indexOf(int[] arr, int ele) {
+        for (int i = 0; i < arr.length; i++)
+            if (arr[i] == ele)
+                return i;
+
+        return -1;
+    }
+    private void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+}
