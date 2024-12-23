e6fbb3a2e833a610798ba0c4c7206c5ac7cdca2e
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2024-12-23 08:46:47 +0530

    Time: 0 ms (100%), Space: 46.4 MB (79.51%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,18 +1,13 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] arr=new int[n+1];
-        for(int i=0;i<=n;i++){
-            int val=0;
-            String s=Integer.toBinaryString(i);
-            System.out.println(s);
-            for(char c:s.toCharArray()){
-                if(c=='1'){
-                    val++;
-                }
-            }
-            arr[i]=val;
-        }
-
-        return arr;
+        var ans = new int[n+1];
+        zip(ans,1,1);
+        return ans;
+    }
+    void zip(int[] ans,int i,int count){
+        if(i >= ans.length) return;
+        ans[i] = count;
+        zip(ans,i << 1,count);
+        zip(ans,(i<<1) + 1,count+1);
     }
 }
