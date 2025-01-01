5c41afd92528171240175d838f6f7b79a5ec8f84
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2025-01-01 07:34:21 +0530

    Time: 2 ms (39.66%), Space: 41.3 MB (77%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.java
@@ -1,20 +1,20 @@
-class Solution {
-    public int maxScore(String s) {
-        int ans=0;
-        for(int i=0;i<s.length()-1;i++){
-            int a=0;
-            for(int j=0;j<=i;j++){
-                if(s.charAt(j)=='0'){
-                    a++;
-                }
-            }
-            for(int j=i+1;j<s.length();j++){
-                if(s.charAt(j)=='1'){
-                    a++;
-                }
-            }
-            ans=Math.max(ans,a);
-        }
-        return ans;
-    }
+class Solution {  
+    public int maxScore(String s) {  
+        int leftscore=0;  
+        int rightscore=0;  
+        int ans=0;  
+
+        for(int i = 0; i < s.length(); i++) {  
+            if(s.charAt(i)=='1') rightscore++;  
+        }  
+
+        for(int i=0;i<s.length()-1;i++) {  
+            if(s.charAt(i)=='0') leftscore++;  
+            if(s.charAt(i)=='1') rightscore--;  
+
+            ans = Math.max(ans,leftscore+rightscore);  
+        }  
+        ans=Math.max(ans,leftscore+rightscore);  
+        return ans;  
+    }  
 }
