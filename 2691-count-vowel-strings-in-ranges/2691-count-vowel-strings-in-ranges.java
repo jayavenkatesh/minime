1c1928fe260846248f2855cb5813bee3e7d84976
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2025-01-02 09:14:28 +0530

    Time: 8 ms (48.29%), Space: 85.9 MB (48.29%) - LeetHub

--- 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
+++ 2691-count-vowel-strings-in-ranges/2691-count-vowel-strings-in-ranges.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] vowelStrings(String[] words, int[][] queries) {
-        int[] arr=new int[words.length];
+        int[] arr=new int[words.length+1];
         Arrays.fill(arr,0);
         HashSet<Character> hs=new HashSet();
         hs.add('a');
@@ -11,22 +11,19 @@ class Solution {
         for(int i=0;i<words.length;i++){
             String s=words[i];
             if(hs.contains(s.charAt(0)) && hs.contains(s.charAt(s.length()-1))){
-                arr[i]=i>0?arr[i-1]+1:1;
+                arr[i+1]=arr[i]+1;
             }
             else{
-                arr[i]=i>0?arr[i-1]:0;
+                arr[i+1]=arr[i];
             }
-        }
-        for(int i:arr){
-            System.out.print(i+" ");
         }
         int[] ans=new int[queries.length];
         int i=0;
         for(int[] q:queries){
             int l=q[0];
             int u=q[1];
-            int z=arr[u];
-            z=l==0?z:z-arr[l-1];
+            int z=arr[u+1];
+            z=l==0?z:z-arr[l];
             ans[i++]=z;
         }
         return ans;
