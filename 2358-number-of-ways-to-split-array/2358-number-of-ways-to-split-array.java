21f595b50fd8ba83e7f0f392cb388f24f000b278
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2025-01-03 07:13:53 +0530

    Time: 3 ms (59.69%), Space: 58.1 MB (82.16%) - LeetHub

--- 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.java
+++ 2358-number-of-ways-to-split-array/2358-number-of-ways-to-split-array.java
@@ -1,11 +1,8 @@
 class Solution {
     public int waysToSplitArray(int[] nums) {
-        if(nums.length>95000 && nums[0]==0 && nums[1]==-1){
-            return 70710;
-        }
         int ans=0;
-        int lefts=nums[0];
-        int rights=0;
+        long lefts=nums[0];
+        long rights=0;
         for(int i=1;i<nums.length;i++){
             rights+=nums[i];
         }
