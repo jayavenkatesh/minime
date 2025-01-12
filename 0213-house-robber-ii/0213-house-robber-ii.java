5d8e6e7d0ed5bd350909295b31a5fb4b14952e20
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2025-01-12 17:57:01 +0530

    Time: 0 ms (100%), Space: 41.8 MB (5.07%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -9,15 +9,6 @@ class Solution {
         
     }
     int fun(int[] nums){
-        if(nums.length==0){
-            return 0;
-        }
-        if(nums.length==1){
-            return nums[0];
-        }
-        if(nums.length==2){
-            return Math.max(nums[0],nums[1]);
-        }
         int prev=nums[0];
         int prev2=0;
         for(int i=1;i<nums.length;i++){
