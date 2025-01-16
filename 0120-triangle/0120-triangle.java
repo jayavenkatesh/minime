cede8df9316e3efe7be46b6e4edef85544ded112
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2025-01-16 16:49:25 +0530

    Time: 4 ms (49.83%), Space: 44.1 MB (73.78%) - LeetHub

--- 0120-triangle/0120-triangle.java
+++ 0120-triangle/0120-triangle.java
@@ -1,25 +1,19 @@
 class Solution {
     int[][] dp;
-    public int minimumTotal(List<List<Integer>> triangle) {
-        dp=new int[triangle.size()][triangle.get(triangle.size()-1).size()];
-        for(int[] a:dp){
-            Arrays.fill(a,-1);
+    public int minimumTotal(List<List<Integer>> tri) {
+        int n=tri.size();
+        int m=tri.get(n-1).size();
+        dp=new int[n][tri.get(n-1).size()];
+        for(int j=0;j<m;j++){
+            dp[n-1][j]=tri.get(n-1).get(j);
         }
-        return rec(0,0,triangle);
-    }
-    int rec(int i,int j,List<List<Integer>> tri){
-        if(i>=tri.size() || j>=tri.get(i).size()){
-            return Integer.MAX_VALUE;
+        for(int i=n-2;i>=0;i--){
+            for(int j=i;j>=0;j--){
+                int down=tri.get(i).get(j)+dp[i+1][j];
+                int dRight=tri.get(i).get(j)+dp[i+1][j+1];
+                dp[i][j]=Math.min(down,dRight);
+            }
         }
-        if(i==tri.size()-1){
-            return tri.get(i).get(j);
-        }
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        int down=tri.get(i).get(j)+rec(i+1,j,tri);
-        int dRight=tri.get(i).get(j)+rec(i+1,j+1,tri);
-
-        return dp[i][j]=Math.min(down,dRight);
+        return dp[0][0];
     }
 }
