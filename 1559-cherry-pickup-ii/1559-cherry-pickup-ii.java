74e08824b79bef2808dee9a3d987e7494f69f452
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2025-01-17 11:57:35 +0530

    Time: 35 ms (23.46%), Space: 46 MB (49.11%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.java
@@ -1,33 +1,34 @@
 class Solution {
-    int[][][] dp;
     public int cherryPickup(int[][] grid) {
-        int m=grid[0].length;
-        int n=grid.length;
-        dp=new int[n][m][m];
-        for(int[][] arr:dp){
-            for(int[] a:arr){
-                Arrays.fill(a,-1);
+        int m = grid[0].length;
+        int n = grid.length;
+        int[][][] dp = new int[n][m][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < m; j++) {
+                dp[n - 1][i][j] = i==j?grid[n - 1][j]:grid[n-1][i]+grid[n-1][j];
             }
         }
-        return rec(0,0,m-1,grid,m);
-    }
-    int rec(int i,int j1,int j2,int[][] grid,int m){
-        if(j1<0 || j1>=m || j2<0 || j2>=m){
-            return (int) -1e7;
-        }
-        if(i==grid.length-1){
-            return j1==j2?grid[i][j1]:grid[i][j1]+grid[i][j2];
-        }
-        if(dp[i][j1][j2]!=-1){
-            return dp[i][j1][j2];
-        }
-        int ans=(int) -1e7;
-        for(int a=-1;a<2;a++){
-            for(int b=-1;b<2;b++){
-                int z=j1==j2?grid[i][j1]:grid[i][j1]+grid[i][j2];
-                ans=Math.max(ans,z+rec(i+1,j1+a,j2+b,grid,m));
+
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j1 = 0; j1 <m; j1++) {
+                for (int j2 = 0; j2 <m; j2++) {
+                    int ans = (int) -1e7;
+                    for (int a = -1; a < 2; a++) {
+                        for (int b = -1; b < 2; b++) {
+                            int z = j1 == j2 ? grid[i][j1] : grid[i][j1] + grid[i][j2];
+                            if(j1+a>=0 && j2+b>=0 && j1+1<m && j2+b<m) {
+                                z+=dp[i + 1][j1 + a][j2 + b];
+                            }
+                            else{
+                                z+=(int) -1e7;
+                            }
+                            ans = Math.max(ans, z);
+                        }
+                    }
+                    dp[i][j1][j2]=ans;
+                }
             }
         }
-        return dp[i][j1][j2]=ans;
+        return dp[0][0][m-1];
     }
 }
