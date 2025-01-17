3421097b6bed83dcf6723600936809e169f2b14e
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2025-01-17 12:02:43 +0530

    Time: 38 ms (18.11%), Space: 44.9 MB (87.65%) - LeetHub

--- 1559-cherry-pickup-ii/1559-cherry-pickup-ii.java
+++ 1559-cherry-pickup-ii/1559-cherry-pickup-ii.java
@@ -2,14 +2,15 @@ class Solution {
     public int cherryPickup(int[][] grid) {
         int m = grid[0].length;
         int n = grid.length;
-        int[][][] dp = new int[n][m][m];
+        int[][] dp = new int[m][m];
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < m; j++) {
-                dp[n - 1][i][j] = i==j?grid[n - 1][j]:grid[n-1][i]+grid[n-1][j];
+                dp[i][j] = i==j?grid[n - 1][j]:grid[n-1][i]+grid[n-1][j];
             }
         }
 
         for (int i = n - 2; i >= 0; i--) {
+            int[][] cur=new int[m][m];
             for (int j1 = 0; j1 <m; j1++) {
                 for (int j2 = 0; j2 <m; j2++) {
                     int ans = (int) -1e7;
@@ -17,7 +18,7 @@ class Solution {
                         for (int b = -1; b < 2; b++) {
                             int z = j1 == j2 ? grid[i][j1] : grid[i][j1] + grid[i][j2];
                             if(j1+a>=0 && j2+b>=0 && j1+1<m && j2+b<m) {
-                                z+=dp[i + 1][j1 + a][j2 + b];
+                                z+=dp[j1 + a][j2 + b];
                             }
                             else{
                                 z+=(int) -1e7;
@@ -25,10 +26,11 @@ class Solution {
                             ans = Math.max(ans, z);
                         }
                     }
-                    dp[i][j1][j2]=ans;
+                    cur[j1][j2]=ans;
                 }
             }
+            dp=cur;
         }
-        return dp[0][0][m-1];
+        return dp[0][m-1];
     }
 }
