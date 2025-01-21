3989b521147c0a0fa9879fe7914e61b7294f75e4
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2025-01-21 15:18:25 +0530

    Time: 16 ms (59.57%), Space: 45.6 MB (13.4%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,27 +1,28 @@
 class Solution {
     int[][] dp;
+
     public int coinChange(int[] coins, int amount) {
-        dp=new int[coins.length][amount+1];
-        for(int[] a:dp){
-            Arrays.fill(a,-1);
+        int n = coins.length;
+        dp = new int[n][amount + 1];
+        for (int t = 0; t <= amount; t++) {
+            if (t % coins[0] == 0)
+                dp[0][t] = t / coins[0];
+            else {
+                dp[0][t] = (int) 1e9;
+            }
         }
-        int ans= rec(coins.length-1,coins,amount);
+        for (int i = 1; i < n; i++) {
+            for (int t = 1; t <= amount; t++) {
+                int notTake = dp[i - 1][t];
+                int take = (int) 1e9;
+                if (coins[i] <= t)
+                    take = 1 + dp[i][t - coins[i]];
+
+                dp[i][t] = Math.min(notTake, take);
+            }
+        }
+        int ans = dp[n-1][amount];
         if(ans>=1e9) return -1;
         return ans;
-    }
-    int rec(int i,int[] coins,int t){
-        if(t==0){
-            return 0;
-        }
-        if(i==0){
-            if(t%coins[i]==0) return t/coins[i];
-            return (int) 1e9;
-        }
-        if(dp[i][t]!=-1) return dp[i][t];
-        int notTake=rec(i-1,coins,t);
-        int take= (int) 1e9;
-        if(coins[i]<=t) take=1+rec(i,coins,t-coins[i]);
-
-        return dp[i][t] = Math.min(notTake,take);
     }
 }
