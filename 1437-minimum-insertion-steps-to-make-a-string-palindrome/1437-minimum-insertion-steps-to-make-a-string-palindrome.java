1becaa060a3c11257491f29250203557951ab15c
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2025-01-23 15:38:20 +0530

    Time: 23 ms (71.13%), Space: 45.2 MB (85.14%) - LeetHub

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minInsertions(String s1) {
+        
+        StringBuilder sb=new StringBuilder(s1);
+        String s2=sb.reverse().toString();
+        int n = s1.length();
+        int m = s2.length();
+        int[] prev=new int[m+1];
+        for (int i = 0; i <= m ; i++) prev[i] = 0;
+        for (int i = 1; i < n + 1; i++) {
+            int[] cur=new int[m+1];
+            for (int j = 1; j <= m; j++) {
+                if (s1.charAt(i-1) == s2.charAt(j-1)) {
+                    cur[j]= 1 + prev[j - 1];
+                }
+                else cur[j] = Math.max(prev[j],cur[j - 1]);
+            }
+            prev=cur;
+        }
+        return n-prev[m];
+    }
+}
