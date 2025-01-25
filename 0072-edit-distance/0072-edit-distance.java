bf9197a0c0b04bb23ef651eaea7f5152315c99bb
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2025-01-25 18:00:54 +0530

    Time: 5 ms (63.47%), Space: 44.6 MB (96.14%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,25 +1,19 @@
 class Solution {
-    int[][] dp;
+    int[] prev;
     public int minDistance(String s1, String s2) {
         int n=s1.length();
         int m=s2.length();
-        dp=new int[n+1][m+1];
-        for(int i=0;i<=n;i++) dp[i][0]=i;
-        for(int i=0;i<=m;i++) dp[0][i]=i;
-
+        prev=new int[m+1];
+        for(int i=0;i<=m;i++) prev[i]=i;
         for(int i=1;i<=n;i++){
+            int[] cur=new int[m+1];
+            cur[0]=i;
             for(int j=1;j<=m;j++){
-                if(s1.charAt(i-1)==s2.charAt(j-1)) dp[i][j] = dp[i-1][j-1];
-                else dp[i][j] = Math.min(1+dp[i-1][j],Math.min(1+dp[i][j-1],1+dp[i-1][j-1]));
+                if(s1.charAt(i-1)==s2.charAt(j-1)) cur[j] = prev[j-1];
+                else cur[j] = Math.min(1+prev[j],Math.min(1+cur[j-1],1+prev[j-1]));
             }
+            prev=cur;
         }
-        return dp[n][m];
-    }
-    int rec(int i,int j,String s1,String s2){
-        if(i<0) return j+1;
-        if(j<0) return i+1;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s1.charAt(i)==s2.charAt(j)) return dp[i][j] = rec(i-1,j-1,s1,s2);
-        return dp[i][j] = Math.min(1+rec(i-1,j,s1,s2),Math.min(1+rec(i,j-1,s1,s2),1+rec(i-1,j-1,s1,s2)));
+        return prev[m];
     }
 }
