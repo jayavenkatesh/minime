931cbaba80b438c61a7a294befd9e6c1a488ede4
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2025-01-25 13:09:29 +0530

    Time: 15 ms (85.29%), Space: 41.5 MB (95.36%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -6,15 +6,11 @@ class Solution {
         dp = new int[m + 1];
         dp[0]=1;
         for (int i = 1; i < n + 1; i++) {
-            int[] cur=new int[m+1];
-            cur[0]=1;
-            for (int j = 1; j < m + 1; j++) {
+            for (int j = m; j >0; j--) {
                 if (s.charAt(i-1) == t.charAt(j-1)) {
-                    cur[j] = dp[j - 1]+dp[j];
+                    dp[j] = dp[j - 1]+dp[j];
                 }
-                else cur[j] = dp[j];
             }
-            dp=cur;
         }
         return dp[m];
     }
