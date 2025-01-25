c1090cb262970bb3d66077d1dae049e42a915efd
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2025-01-25 12:56:29 +0530

    Time: 20 ms (44.1%), Space: 50.9 MB (75.86%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,19 +1,22 @@
 class Solution {
     int[][] dp;
     public int numDistinct(String s, String t) {
-        int n=s.length();
-        int m=t.length();
-        dp=new int[n][m];
-        for(int[] a:dp) Arrays.fill(a,-1);
-        return rec(n-1,m-1,s,t);
-    }
-    int rec(int i,int j,String s1,String s2){
-        if(j<0) return 1;
-        if(i<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s1.charAt(i)==s2.charAt(j)){
-            return dp[i][j] = rec(i-1,j-1,s1,s2)+rec(i-1,j,s1,s2);
+        int n = s.length();
+        int m = t.length();
+        dp = new int[n + 1][m + 1];
+        for (int i = 0; i < n + 1; i++)
+            dp[i][0] = 1;
+        for (int i = 1; i < m + 1; i++)
+            dp[0][i] = 0;
+
+        for (int i = 1; i < n + 1; i++) {
+            for (int j = 1; j < m + 1; j++) {
+                if (s.charAt(i-1) == t.charAt(j-1)) {
+                    dp[i][j] = dp[i - 1][j - 1]+dp[i - 1][j];
+                }
+                else dp[i][j] = dp[i - 1][j];
+            }
         }
-        return dp[i][j] = rec(i-1,j,s1,s2);
+        return dp[n][m];
     }
 }
