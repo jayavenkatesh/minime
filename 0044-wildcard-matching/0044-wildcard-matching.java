18fa3360e8287eff956d0738432b2102b8d32a1b
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2025-01-25 19:52:50 +0530

    Time: 23 ms (37.84%), Space: 44.9 MB (53.49%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,38 +1,29 @@
 class Solution {
-    int[][] dp;
-    public boolean isMatch(String s, String p) {
-        int n=s.length();
-        int m=p.length();
-        dp=new int[n][m];
-        for(int[] a:dp) Arrays.fill(a,-1);
-        return rec(n-1,m-1,s,p);
-    }
-    boolean rec(int i,int j,String s1,String s2){
-        if(i<0 && j<0) return true;
-        if(i>=0 && j<0) return false;
-        
-        if(i<0 && j>=0){
-            for(int a=0;a<=j;a++){
-                if(s2.charAt(a)!='*') return false;
+    boolean[][] dp;
+
+    public boolean isMatch(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        dp = new boolean[n + 1][m + 1];
+        dp[0][0] = true;
+        for (int j = 1; j <= m; j++) {
+            if (s2.charAt(j - 1) == '*') {
+                dp[0][j] = dp[0][j - 1];
             }
-            return true;
         }
-        if(dp[i][j]!=-1) return dp[i][j]==0?false:true;
-        if((s1.charAt(i)==s2.charAt(j)) || s2.charAt(j)=='?') {
-            boolean z=rec(i-1,j-1,s1,s2);
-            if(z) dp[i][j]=1;
-            else dp[i][j]=0;
-            return z;
-        } 
-        if(s2.charAt(j)=='*'){
-            boolean z= (rec(i,j-1,s1,s2) || rec(i-1,j,s1,s2));
-            if(z) dp[i][j]=1;
-            else dp[i][j]=0;
-            return z;
-        } 
-
-        dp[i][j]=0;
-        return false;
-
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if ((s1.charAt(i-1) == s2.charAt(j-1)) || s2.charAt(j-1) == '?') {
+                    dp[i][j] = dp[i - 1][j - 1];
+                }
+                else if(s2.charAt(j-1) == '*') {
+                    dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                }
+                else{
+                    dp[i][j] = false;
+                }
+            }
+        }
+        return dp[n][m];
     }
 }
