18bab50a340f5c6613cc9025528dbdde34951a3a
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2025-02-02 10:33:01 +0530

    Time: 10 ms (97.95%), Space: 42.4 MB (83.71%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
@@ -1,25 +1,26 @@
 class Solution {
-    int[][] dp;
     public int minCost(int N, int[] cuts) {
-        int n=cuts.length+2;
-        int[] arr=new int[n];
-        arr[0]=0;
-        arr[n-1]=N;
-        int j=1;
-        for(int i=0;i<cuts.length;i++) arr[j++]=cuts[i];
+        int n = cuts.length + 2;
+        int[] arr = new int[n];
+        arr[0] = 0;
+        arr[n - 1] = N;
+        int z = 1;
+        for (int i = 0; i < cuts.length; i++)
+            arr[z++] = cuts[i];
         Arrays.sort(arr);
-        dp=new int[n][n];
-        for(int[] a:dp) Arrays.fill(a,-1);
-        return rec(1,n-2,arr);
-    }
-    int rec(int i,int j,int[] arr){
-        if(i>j) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int min=(int) 1e9;
-        for(int k=i;k<=j;k++){
-            int cuts=arr[j+1]-arr[i-1]+rec(i,k-1,arr)+rec(k+1,j,arr);
-            min=Math.min(min,cuts);
+        int[][] dp = new int[n][n];
+        for (int i = n-2; i >= 1; i--) {
+            for (int j = 1; j <= n-2; j++) {
+                if (i > j)
+                    continue;
+                int min = (int) 1e9;
+                for (int k = i; k <= j; k++) {
+                    int cutss = arr[j + 1] - arr[i - 1] + dp[i][k - 1] + dp[k + 1][j];
+                    min = Math.min(min, cutss);
+                }
+                dp[i][j] = min;
+            }
         }
-        return dp[i][j] = min;
+        return dp[1][n-2];
     }
 }
