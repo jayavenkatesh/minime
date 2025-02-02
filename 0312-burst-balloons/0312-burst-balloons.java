069c2d9efe2174e7796208526f3f24e1659e0ee2
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2025-02-02 11:49:06 +0530

    Time: 31 ms (98.23%), Space: 42.7 MB (52.89%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -6,20 +6,20 @@ class Solution {
         int[] arr=new int[n];
         arr[0]=1;
         arr[n-1]=1;
-        int j=1;
-        for(int i=0;i<cuts.length;i++) arr[j++]=cuts[i];
-        dp=new int[n][n];
-        for(int[] a:dp) Arrays.fill(a,-1);
-        return rec(1,n-2,arr);
-    }
-    int rec(int i,int j,int[] arr){
-        if(i>j) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        int min=(int) -1e9;
-        for(int k=i;k<=j;k++){
-            int cuts=arr[j+1]*arr[i-1]*arr[k]+rec(i,k-1,arr)+rec(k+1,j,arr);
-            min=Math.max(min,cuts);
+        int z=1;
+        for(int i=0;i<cuts.length;i++) arr[z++]=cuts[i];
+        dp=new int[n+1][n+1];
+        for(int i=n-2;i>=1;i--){
+            for(int j=1;j<=n-2;j++){
+                if(i>j) continue;
+                int min=(int) -1e9;
+                for(int k=i;k<=j;k++){
+                    int cutss=arr[j+1]*arr[i-1]*arr[k]+dp[i][k-1]+dp[k+1][j];
+                    min=Math.max(min,cutss);
+                }
+                dp[i][j] = min;
+            }
         }
-        return dp[i][j] = min;
+        return dp[1][n-2];
     }
 }
