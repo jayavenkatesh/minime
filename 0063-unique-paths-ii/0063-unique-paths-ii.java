eb99c079e1cff9381b1ddd1c097cbc705f3cb56c
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2025-02-03 10:24:13 +0530

    Time: 0 ms (100%), Space: 41.7 MB (79.95%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,22 +1,27 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-        return mazeObstacles(obstacleGrid.length,obstacleGrid[0].length,obstacleGrid);
-    }
-    static int mazeObstacles(int n, int m, int[][] mat) {
+    public int uniquePathsWithObstacles(int[][] mat) {
+        int n=mat.length;
+        int m=mat[0].length;
         if(mat[0][0]==1) return 0;
-        int[][] dp=new int[n][m];
-		dp[0][0]=1;
-		for(int i=0;i<n;i++){
-			for(int j=0;j<m;j++){
-				if(i==0 && j==0) continue;
-                if(mat[i][j]==1) continue;
-				int up=0;
-				int left=0;
-				if(i!=0) up=dp[i-1][j];
-				if(j!=0) left=dp[i][j-1];
-				dp[i][j]=up+left;
+        int[] dp=new int[m+1];
+		dp[1]=1;
+		for(int i=1;i<=n;i++){
+            int[] cur=new int[m+1];
+			for(int j=1;j<=m;j++){
+				if(i==1 && j==1) {
+                    cur[j]=1;
+                    continue;
+                }
+                if(mat[i-1][j-1]==1){
+                    cur[j]=0;
+                    continue;
+                }
+				int up=dp[j];
+				int left=cur[j-1];
+				cur[j]=up+left;
 			}
+            dp=cur;
 		}
-		return dp[n-1][m-1];
-	}
+		return dp[m];
+    }
 }
