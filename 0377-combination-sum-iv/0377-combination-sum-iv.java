3439d4831758f0db7d869e6bd48de724761ec6e9
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2025-02-03 12:24:09 +0530

    Time: 16 ms (3.29%), Space: 41.5 MB (12.66%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.java
+++ 0377-combination-sum-iv/0377-combination-sum-iv.java
@@ -1,18 +1,17 @@
 class Solution {
     int[][] dp;
     public int combinationSum4(int[] nums, int target) {
-        dp = new int[nums.length][target + 1];
-        for (int[] a : dp) Arrays.fill(a, -1);
-        return rec(nums.length - 1, nums, target);
-    }
-    int rec(int i, int[] nums, int t) {
-        if (t == 0) return 1;
-        if (i < 0) return 0;
-        if (dp[i][t] != -1) return dp[i][t];
-        int ways = 0;
-        for (int j = 0; j < nums.length; j++) {
-            if (nums[j] <= t) ways += rec(nums.length - 1, nums, t - nums[j]);
+        dp = new int[nums.length+1][target + 1];
+        for(int i=0;i<nums.length+1;i++) dp[i][0]=1;
+        for(int i=1;i<nums.length+1;i++){
+            for(int t=1;t<target+1;t++){
+                int ways = 0;
+                for (int j = 0; j < nums.length; j++) {
+                    if (nums[j] <= t) ways += dp[nums.length - 1][t - nums[j]];
+                }
+                dp[i][t] = ways;
+            }
         }
-        return dp[i][t] = ways;
+        return dp[nums.length][target];
     }
 }
