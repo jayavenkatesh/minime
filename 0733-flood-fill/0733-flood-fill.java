f3a622e4497b4c9ce3c0fc06610be7d0155942c2
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2025-02-05 08:05:29 +0530

    Time: 0 ms (100%), Space: 44.7 MB (96.47%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,21 +1,21 @@
 class Solution {
+    int[] dr={-1,0,1,0};
+    int[] dc={0,1,0,-1};
+    int a=0;
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int a=image[sr][sc];
+        a=image[sr][sc];
         image[sr][sc]=color;
-        boolean[][] visited =new boolean[image.length][image[0].length];
-        dfs(sr,sc,image,color,visited,a);
+        if(a==color) return image;
+        dfs(sr,sc,image,color);
         return image;
     }
-    void dfs(int i,int j,int[][] image,int col,boolean[][] vis,int a){
-        vis[i][j]=true;
-        int[] dr={-1,0,1,0};
-        int[] dc={0,1,0,-1};
+    void dfs(int i,int j,int[][] image,int col){
+        image[i][j]=col;
         for(int z=0;z<4;z++){
             int r=dr[z]+i;
             int c=dc[z]+j;
-            if(r>=0 && r<image.length && c>=0 && c<image[0].length && !vis[r][c] && image[r][c]==a){
-                image[r][c]=col;
-                dfs(r,c,image,col,vis,a);
+            if(r>=0 && r<image.length && c>=0 && c<image[0].length && image[r][c]==a){
+                dfs(r,c,image,col);
             }
         }
     }
