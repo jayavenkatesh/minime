1c496b64e92886f3cd1b57574b307a3578dd92c0
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2025-02-07 18:19:13 +0530

    Time: 7 ms (50.44%), Space: 46.2 MB (13.92%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,38 +1,34 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> list=new ArrayList();
-        for(int i=0;i<numCourses;i++){
-            list.add(new ArrayList());
-
+    public boolean canFinish(int N, int[][] prerequisites) {
+        List<List<Integer>> adj=new ArrayList();
+        for(int i=0;i<N;i++){
+            adj.add(new ArrayList());
         }
-        int visited[]=new int[numCourses];
+        for(int[] a:prerequisites){
+            adj.get(a[0]).add(a[1]);
+        }
         
-        for(int i=0;i<prerequisites.length;i++){
-            int[] arr=prerequisites[i];
-            list.get(arr[0]).add(arr[1]);
-            
-
+        
+        int[] indnum=new int[N];
+        for(int i=0;i<N;i++){
+            for(int j:adj.get(i)){
+                indnum[j]++;
+            }
         }
-        for(int i=0;i<list.size();i++){
-            if(!dfs(i,list,visited)){
-                return false;
-            }   
+        Queue<Integer> queue=new LinkedList();
+        for(int i=0;i<N;i++){
+            if(indnum[i]==0) queue.offer(i);
         }
-        return true;
-    }
-    boolean dfs(int i,List<List<Integer>> list,int[] visited){
-        if(visited[i]==1){
-            return false;
+        List<Integer> topo=new ArrayList();
+        while(!queue.isEmpty()){
+            int a=queue.poll();
+            topo.add(a);
+            for(int z:adj.get(a)){
+                indnum[z]--;
+                if(indnum[z]==0) queue.offer(z);
+            }
         }
-        if(visited[i]==2){
-            return true;
-        }
-        visited[i]=1;
-        for(int n:list.get(i)){
-            if(!dfs(n,list,visited))
-                return false;
-        }
-        visited[i]=2;
-        return true;
+        if(topo.size()==N) return true;
+        return false;
     }
 }
