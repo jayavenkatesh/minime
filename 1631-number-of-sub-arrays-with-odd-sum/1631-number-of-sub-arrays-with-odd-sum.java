acbd37c389a09a5622f1aa68df902aa53a6643c9
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2025-02-26 08:00:12 +0530

    Time: 7 ms (82.59%), Space: 58.2 MB (43.74%) - LeetHub

---
+++ 1631-number-of-sub-arrays-with-odd-sum/1631-number-of-sub-arrays-with-odd-sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int numOfSubarrays(int[] arr) {
+        int mod = 1_000_000_007;
+        int oddCount = 0, evenCount = 1;
+        int sum = 0, result = 0;
+        for (int num : arr) {
+            sum += num;
+            if (sum % 2 == 0) {
+                result = (result + oddCount) % mod;
+                evenCount++;
+            } else {
+                result = (result + evenCount) % mod;
+                oddCount++;
+            }
+        }
+        return result;
+    }
+}
