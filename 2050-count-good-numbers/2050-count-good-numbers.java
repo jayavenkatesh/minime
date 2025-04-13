97bfca02917adf49d9252f9967510970c9ade05c
Ch Jaya Venkatesh <118626954+jayavenkatesh@users.noreply.github.com>
2025-04-13 07:12:13 +0530

    Time: 0 ms (100%), Space: 41.4 MB (5.1%) - LeetHub

--- 2050-count-good-numbers/2050-count-good-numbers.java
+++ 2050-count-good-numbers/2050-count-good-numbers.java
@@ -1,5 +1,25 @@
 class Solution {
+    final long MOD = 1000000007;
+
     public int countGoodNumbers(long n) {
+        long evenChoices = 5;
+        long oddChoices = 4;
+        long result = powerMod(evenChoices, (n + 1) / 2) * powerMod(oddChoices, n / 2) % MOD;
         
+        return (int) result;
+    }
+    private long powerMod(long base, long exponent) {
+        long result = 1;
+        base %= MOD;
+        
+        while (exponent > 0) {
+            if (exponent % 2 == 1)
+                result = (result * base) % MOD;
+            
+            exponent >>= 1;
+            base = (base * base) % MOD;
+        }
+        
+        return result;
     }
 }
